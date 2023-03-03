06334db3f205f9d9d64e1410f43570227f7630db
Dimitris Zafeiris <kremou115@gmail.com>
2023-03-03 17:36:26 +0200

    Added favicons,Team section in About page,added a new object teamusers with our all infos

---
+++ src/components/Team.js
@@ -0,0 +1,37 @@
+import React,{useState,useEffect} from 'react';
+import TeamUserCard from './TeamUserCard';
+
+export default function Team(){
+    const UserApi = 'http://localhost:3001/teamusers/';
+    const [teamusers,setTeamusers] = useState(null);
+
+
+    const getTeamUser = () =>{
+        fetch(UserApi)
+        .then((res)=> res.json())
+        .then((data) =>{
+            setTeamusers(data);
+        });
+    }
+
+    useEffect(()=>{
+        getTeamUser()
+    },[]);
+
+    if (!teamusers) return <div>No one team user </div>
+    return (
+        <>
+        <section className="bg-white dark:bg-gray-900 flex flex-col">
+        <div className="container px-6 py-10 mx-auto">
+        <h1 className="text-2xl font-semibold text-center text-indigo-600 capitalize lg:text-3xl dark:text-white border-t-4 py-2 border-indigo-500">Our Team</h1>
+        <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 xl:grid-cols-4 mb-4 ">
+            {teamusers.length && 
+            teamusers.map((user) =>{ 
+                return(<TeamUserCard key={user.fname} fname={user.fname} lname={user.lname} title={user.title} linkedin={user.linkedin} email={user.email} github={user.github} bio={user.bio}/>)
+            })}
+            </div>
+        </div>
+        </section>
+        </>
+    )
+}
---
+++ src/components/TeamUserCard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faCodeCompare,faAt,faLink} from '@fortawesome/free-solid-svg-icons'
+export default function TeamUserCard({title,fname,lname,bio,github,linkedin,email}){
+
+
+    return(
+        <>
+         <div className="flex flex-col items-center p-8 transition-colors duration-300 transform border cursor-pointer rounded-xl hover:border-transparent group hover:bg-indigo-600 dark:border-slate-700 dark:hover:border-transparent">
+                <img className="object-cover w-32 h-32 rounded-full ring-4 ring-slate-300" src={`${github}.png`} alt={`${fname} ${lname}`} title={`${fname} ${lname}`}/>
+                <h1 className="mt-4 text-2xl font-semibold text-slate-700 capitalize dark:text-white group-hover:text-white">{fname} {lname}</h1>
+                <p className="mt-2 text-slate-500 capitalize dark:text-slate-300 group-hover:text-slate-300">{title}</p>
+                <p className="mt-2 text-slate-500 capitalize dark:text-slate-300 group-hover:text-slate-300">{bio}</p>
+                <div className="flex mt-3 -mx-2 ">
+                    <a href={`mailto:${email}`} title="email" className="mx-2 text-slate-600 dark:text-slate-300 hover:text-slate-500 dark:hover:text-slate-300 group-hover:text-white" aria-label="Email">
+                        
+                    <FontAwesomeIcon icon={faAt} />
+                    </a>
+                    <a href={linkedin} title="linkedin" className="mx-2 text-slate-600 dark:text-slate-300 hover:text-slate-500 dark:hover:text-slate-300 group-hover:text-white" aria-label="LinkedIn">
+                    <FontAwesomeIcon icon={faLink} />
+                    </a>
+                    <a href={github} title="github" className="mx-2 text-slate-600 dark:text-slate-300 hover:text-slate-500 dark:hover:text-slate-300 group-hover:text-white" aria-label="Github">
+                        <FontAwesomeIcon icon={faCodeCompare} />
+                    </a>
+                </div>
+                </div>
+        </>
+
+    )
+
+}
