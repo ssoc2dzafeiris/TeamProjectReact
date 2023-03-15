892d0e80369d9857dad0674732bf9239d3852239
Dimitris Zafeiris <118735106+ssoc2dzafeiris@users.noreply.github.com>
2023-03-15 18:27:01 +0200

    Shadow-sm added at buttons edit/delete

--- src/components/Utilities/Buttons/buttondelete.js
+++ src/components/Utilities/Buttons/buttondelete.js
@@ -14,7 +14,7 @@ export default function ButtonDelete(props){
 
     return (
         <>
-        <button onClick={() => openModal()} className="text-white bg-red-600 hover:bg-gray-900 hover:ease-out hover:duration-300 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
+        <button onClick={() => openModal()} className="text-white bg-red-600 hover:bg-gray-900 hover:ease-out hover:duration-300 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 shadow-sm">
         <i class="fa-solid fa-trash-can"></i> Delete
         </button>
             <DelleteMessage idNumber={props.idNumber} closeModal={closeModal} open={isModalOpen} close={closeModal} />
--- src/components/Utilities/Buttons/buttonedit.js
+++ src/components/Utilities/Buttons/buttonedit.js
@@ -7,7 +7,7 @@ export default function ButtonEdit(props){
     function openModal() {setIsModalOpen(true);}
     return (
        <div style={{ position: 'relative', zIndex: '1' }}>
-            <button onClick={() => openModal()} className="text-white  bg-indigo-400 border border-gray-300  hover:bg-white hover:ease-out hover:duration-300 hover:text-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
+            <button onClick={() => openModal()} className="text-white  bg-indigo-400 border border-gray-300  hover:bg-white hover:ease-out hover:duration-300 hover:text-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 shadow-sm">
             <i class="fa-solid fa-pen-to-square"></i> Edit</button>
             <EditPage fetchDetails={props.fetchDetails}  data={props.data} idNumber={props.idNumber} open={isModalOpen} close={closeModal} />
         </div>
