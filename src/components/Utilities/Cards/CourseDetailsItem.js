92a147d3c88c7a101f69984ce040db5c9f5920f1
Dimitris Zafeiris <118735106+ssoc2dzafeiris@users.noreply.github.com>
2023-03-15 18:02:28 +0200

    Refactoring the CourseDetails
    - Refactor the li element with a new Component CourseDetailsItem

--- src/components/Utilities/Cards/CourseDetailsItem.js
+++ src/components/Utilities/Cards/CourseDetailsItem.js
@@ -5,9 +5,9 @@ export default function CourseDetailsItem({IconTitle,heading,content}){
         <li className="py-2 flex flex-col">
             <div className="flex items-center">
             <div>
-                <span className="text-normal font-normal inline-block py-1 px-2 rounded-full text-slate-100 bg-indigo-600 mr-3"><i className={IconTitle}></i></span></div>
+                <span className="text-normal font-normal inline-block py-1 px-2 rounded-full text-gray-100 bg-indigo-600 mr-3 "><i className={IconTitle}></i></span></div>
             <div>
-                <h4 className="text-slate-600 hover:text-indigo-500 font-normal hover:underline text-normal">{heading}: <span>{content}</span></h4>
+                <h4 className="text-gray-600 hover:text-indigo-500 font-normal text-normal hover:border-b-2 hover:border-indigo-400">{heading}: <span className="font-bold">&nbsp; {content}</span></h4>
             </div>
             </div>
         </li>
