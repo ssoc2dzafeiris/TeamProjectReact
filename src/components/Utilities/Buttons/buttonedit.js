36b83aa9d8b314ab99535af8343519f5d3531dee
ssoc2skatsaberis <118774780+ssoc2skatsaberis@users.noreply.github.com>
2023-03-17 00:35:02 +0200

    connect buttonEdit to AddCourse
    edit the file to link to AddCourse, that works like Edit

--- src/components/Utilities/Buttons/buttonedit.js
+++ src/components/Utilities/Buttons/buttonedit.js
@@ -1,15 +1,22 @@
 import { useState } from "react";
 import EditPage from "../../editPage";
+import { Link } from 'react-router-dom';
 
 export default function ButtonEdit(props){
     const [isModalOpen, setIsModalOpen] = useState(false);
     function closeModal() {setIsModalOpen(false);}
     function openModal() {setIsModalOpen(true);}
+
+    const propsData = {from:'Edit', data : props.data, idNumber:props.idNumber}
+
+
     return (
        <div style={{ position: 'relative', zIndex: '1' }}>
+             <Link to={`/addcourse/`} state={propsData} >
             <button onClick={() => openModal()} className="text-white  bg-indigo-400 border border-gray-300  hover:bg-white hover:ease-out hover:duration-300 hover:text-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">
             <i class="fa-solid fa-pen-to-square"></i> Edit</button>
-            <EditPage fetchDetails={props.fetchDetails}  data={props.data} idNumber={props.idNumber} open={isModalOpen} close={closeModal} />
+           
+            </Link>
         </div>
     );
 }
